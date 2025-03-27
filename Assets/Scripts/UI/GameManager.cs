eca2a4a04cd4fa58f59fd36ed98116d35a838fe0
Erik Diers <erik.diers@ruhr-uni-bochum.de>
2025-03-27 17:11:35 +0100

    UI

---
+++ Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    private static int currentLevel = 0;
+
+    [SerializeField] private Level[] levels;
+    
+    [SerializeField] private TextMeshProUGUI saladProgress;
+    [SerializeField] private TextMeshProUGUI carrotProgress;
+    [SerializeField] private TextMeshProUGUI pumpkinProgress;
+
+    [SerializeField] private Image timePassedProgressBar;
+
+    private int saladAmount = -1;
+    private int carrotAmount = -1;
+    private int pumpkinAmount = -1;
+
+    private float levelStartedTime;
+
+    private void Awake() {
+        Instance = this;
+        levelStartedTime = Time.time;
+
+        AddSalad();
+        AddCarrot();
+        AddPumpkin();
+    }
+
+    public void Update() {
+        float progress = (Time.time - levelStartedTime) / levels[currentLevel].dayTimeSeconds;
+        timePassedProgressBar.fillAmount = progress;
+        
+        if (progress >= 1f) {
+            Debug.Log("Time up!");
+        }
+    }
+
+    public void AddSalad() {
+        saladAmount++;
+
+        saladProgress.text = saladAmount + "/" + levels[currentLevel].targetSalad;
+        if (saladAmount >= levels[currentLevel].targetSalad)
+            saladProgress.color = Color.green;
+    }
+
+    public void AddCarrot() {
+        carrotAmount++;
+
+        carrotProgress.text = carrotAmount + "/" + levels[currentLevel].targetCarrot;
+        if (carrotAmount >= levels[currentLevel].targetCarrot)
+            carrotProgress.color = Color.green;
+    }
+
+    public void AddPumpkin() {
+        pumpkinAmount++;
+
+        pumpkinProgress.text = pumpkinAmount + "/" + levels[currentLevel].targetPumpkin;
+        if (pumpkinAmount >= levels[currentLevel].targetPumpkin)
+            pumpkinProgress.color = Color.green;
+    }
+}
+
+[Serializable]
+public class Level {
+    public int targetSalad = 5;
+    public int targetCarrot = 5;
+    public int targetPumpkin = 5;
+
+    public int dayTimeSeconds = 300;
+}
