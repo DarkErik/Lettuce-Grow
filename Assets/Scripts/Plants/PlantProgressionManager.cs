f651880b901be64174dc54fc40f85c0951081826
Real-Gollum <olikoopa@gmail.com>
2025-03-30 16:01:07 +0200

    Assigned the proper item for each plantneed

--- Assets/Scripts/Plants/PlantProgressionManager.cs
+++ Assets/Scripts/Plants/PlantProgressionManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(FlowerpotBaseLogic))]
 public class PlantProgressionManager : MonoBehaviour
 {
+
     [SerializeField] private GameObject minigameGameObject; //MinigameFrame
     private MinigameFrame minigameFrame;
     [SerializeField] private GameObject bubbleGameObject; // PlantsNeedsBouble
@@ -13,6 +14,7 @@ public class PlantProgressionManager : MonoBehaviour
     [SerializeField] private GameObject dustCloudPrefab;
 
     [SerializeField] private ProgressBarUI progressBarUI;
+
 
     private FlowerpotBaseLogic flowerpotBaseLogic;
     private PlantSO plantData;
