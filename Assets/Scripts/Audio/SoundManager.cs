dc522e17107de77b11eaba28a2801fecaea11d29
TheREALSenate <Michael.Stach@ruhr-uni-bochum.de>
2025-03-30 11:59:47 +0200

    Erik didnt like our sounds and thus i had to use serious ones :(

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -7,66 +7,87 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance { get; private set; }
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private const string homeBrewPlayerPrefsKey = "isHomeBrewActive";
+    private bool isHomeBrewActive;
+
     private float masterVolume = 1f;
 
     private void Awake()
     {
         Instance = this;
+        isHomeBrewActive = (PlayerPrefs.GetInt(homeBrewPlayerPrefsKey) != 0);
+    }
+
+    public void ChangeHomeBrewMode(bool isHomeBrewActive) {
+        this.isHomeBrewActive = isHomeBrewActive;
+        PlayerPrefs.SetInt(homeBrewPlayerPrefsKey, (this.isHomeBrewActive ? 1 : 0));
     }
 
     public void PlayFliegenklatscheSound(Vector3 position, float volume = 1f) {
-        PlaySound(audioClipRefsSO.klatsch, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.klatschHomeBrew : audioClipRefsSO.klatsch;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayPlantNeedArisesSound(Vector3 position, float volume = 1f) {
-        PlaySound(audioClipRefsSO.plantNeedArises, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.plantNeedArisesHomeBrew : audioClipRefsSO.plantNeedArises;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayPlantSellSound(Vector3 position, float volume = 1f) {
-        PlaySound(audioClipRefsSO.plantSold, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.plantSoldHomeBrew : audioClipRefsSO.plantSold;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayWaterHitPlantSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.platsch, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.platschHomeBrew : audioClipRefsSO.platsch;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayBanjoHitSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.banjoHit, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.banjoHitHomeBrew : audioClipRefsSO.banjoHit;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayBanjoMissedSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.banjoMissed, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.banjoMissedHomeBrew : audioClipRefsSO.banjoMissed;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayPlantPlantedSound(Vector3 position, float volume = 1f) {
-        PlaySound(audioClipRefsSO.plantPlanted, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.plantPlantedHomeBrew : audioClipRefsSO.plantPlanted;
+        PlaySound(audioClips, position, volume);
     }
     public void PlayHarvestSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.harvest, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.harvestHomeBrew : audioClipRefsSO.harvest;
+        PlaySound(audioClips, position, volume);
     }
     
     public void PlayPlantDeathSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.plantDeath, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.plantDeathHomeBrew : audioClipRefsSO.plantDeath;
+        PlaySound(audioClips, position, volume);
     }
     
 
     public void PlayPlantFinishedGrowingSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.plantFinishedGrowing, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.plantFinishedGrowingHomeBrew : audioClipRefsSO.plantFinishedGrowing;
+        PlaySound(audioClips, position, volume);
     }
 
     public void PlayItemGrabbedSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.itemGrabbed, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.itemGrabbedHomeBrew : audioClipRefsSO.itemGrabbed;
+        PlaySound(audioClips, position, volume);
     }
     public void PlayItemPlacedSound(Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipRefsSO.itemPlaced, position, volume);
+        AudioClip[] audioClips = isHomeBrewActive ? audioClipRefsSO.itemPlacedHomeBrew : audioClipRefsSO.itemPlaced;
+        PlaySound(audioClips, position, volume);
     }
     public void PlayFootstepSound(Vector3 position, float volume = 1f)
     {
